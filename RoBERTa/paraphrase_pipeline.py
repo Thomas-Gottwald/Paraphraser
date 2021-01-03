48bfc145a3e7220000e5a6e1f73878f7992eb6e4
Thomas Gottwald <thomas.gottwald@uni-wuppertal.de>
2021-01-03 11:06:59 +0100

    universel bos and eos tokens used

--- RoBERTa/paraphrase_pipeline.py
+++ RoBERTa/paraphrase_pipeline.py
@@ -193,8 +193,10 @@ class ParaphrasePipeline():
                 count += 1
         newText = self.tokenizer.decode(tokens)
 
-        startToken = "AddedToken(content='<s>', single_word=False, lstrip=False, rstrip=False, normalized=True) "
-        endToken = " AddedToken(content='</s>', single_word=False, lstrip=False, rstrip=False, normalized=True)"
+        # startToken = "AddedToken(content='<s>', single_word=False, lstrip=False, rstrip=False, normalized=True) "
+        # endToken = " AddedToken(content='</s>', single_word=False, lstrip=False, rstrip=False, normalized=True)"
+        startToken = self.tokenizer.bos_token
+        endToken = self.tokenizer.eos_token
         if startEndToken:
             newText = newText.replace(startToken, '<s> ')
             newText = newText.replace(endToken, ' </s>')
