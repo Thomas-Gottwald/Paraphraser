8385bfd2676fec93364b19ac0ae78d566d110216
Thomas Gottwald <thomas.gottwald@uni-wuppertal.de>
2021-01-26 15:47:53 +0100

    updated the sampling skript

--- RoBERTa/getParaphrasedSample.py
+++ RoBERTa/getParaphrasedSample.py
@@ -26,35 +26,31 @@ def get_paraphraser(device=0, input_window_size=512):
 
     return paraphraser
 
-def spin_text(paraphraser, og_path, sp_path, mask_pc=0.1, add_args={'range_replace' : (1, 4)}):
+def spin_text(paraphraser, og_path, para_args={'range_replace' : (1, 4)}):
     """
-    uses the given paraphraser to spin the text in og_path and write it to sp_path or return it if sp_path == None
+    uses the given paraphraser to spin the text in og_path and return the spun_text
     """
     with open(og_path, encoding='utf-8') as file:
         originalText = file.read()
-    spun_text = paraphraser.parapherase(originalText, mask=mask_pc, **add_args)
-    if sp_path != None:
-        with open(sp_path, 'w', encoding='utf-8', newline='\n') as file:
-            file.write(spun_text)
-    else:
-        return spun_text
+    spun_text = paraphraser.parapherase(originalText, **para_args)
+ 
+    return spun_text
 
-# determents whether the paraphrased text are loaded from the data if they exist or created new
-load_spun_form_data = False
-# additional arguments for the paraphraser (only used with load_spun_form_data = False)
-paraphrase_args = {'range_replace' : [(1, 4), (0, 4)], 'use_score': False, 'mark_replace': False, 'return_df': False, 'startEndToken': False}
 # the size of the sample
-N = 20
+N = 3
 # from witch data should be sampled
 data = [Data.WIKIPEDIA]
-# witch amounts (in %) of paraphrased words should the sample text contain
-para_pc = {0, 10, 20, 30}
+# arguments for the paraphraser
+# (return_df=True is only aviable for load_spun_form_data=False and if it is True for all args)
+paraphrase_args = [{'mask' : 0.1, 'range_replace' : [(1, 4), (0, 4)], 'return_df': True},
+                   {'mask' : 0.2, 'range_replace' : [(1, 4), (0, 4)], 'return_df': True},
+                   {'mask' : 0.3, 'range_replace' : [(1, 4), (0, 4)], 'return_df': True}]
+# determents whether the paraphrased text are loaded from the data created new
+load_spun_form_data = False
+# determents whether it is hidden if a text was paraphrased
+disguise_sample = False
 # the name of the folder to stor this sample
 sampleFolder = datetime.now().strftime("%Y-%m-%d %H_%M_%S")
-
-# make sure that the original files are in the sample
-if 0 not in para_pc:
-    para_pc.add(0)
 
 # set the paths to the data
 path = get_local_path()
@@ -67,7 +63,7 @@ for d in data:
     elif d == Data.WIKIPEDIA:
         data_paths.append(os.path.join(path, *['data', 'wikipedia']))
 if len(data_paths) == 0:
-    print('data is not specificied!')
+    print('Data is not specificied!')
     quit()
 
 # get the original text file names
@@ -86,17 +82,24 @@ for i in range(N):
 if not os.path.exists(os.path.join(path, *['data', 'sample', sampleFolder])):
     os.makedirs(os.path.join(path, *['data', 'sample', sampleFolder]))
 
-# check if all the spun text directories exist (if not create them so that the spun files can be created)
-if load_spun_form_data:
-    for dp in data_paths:
-        for pc in para_pc:
-            if pc != 0 and not os.path.exists(os.path.join(dp, 'sp({}%)'.format(pc))):
-                os.makedirs(os.path.join(dp, 'sp({}%)'.format(pc)))
+# check if return_df is ether True or False (dose not exist) for all paraphrase_args
+return_df = False
+count_df = 0
+for arg in paraphrase_args:
+    if 'return_df' in list(arg.keys()) and arg['return_df']:
+        count_df += 1
+if count_df == len(paraphrase_args) and not load_spun_form_data:
+    return_df = True
+elif count_df != 0 or load_spun_form_data:
+    print('Return_df=True can only be used with load_from_data=False and ruturn_df=True for all args!')
+    quit()
 
-# stores the information in witch order the text occur in the sample files 
-logInfo = []
+if disguise_sample:
+    # stores the information in witch order the text occur in the sample files (for disguise_sample=True)
+    logInfo = []
 
-paraphraser = None
+# paraphraser = None
+# if not load_spun_from data set up the paraphraser
 if not load_spun_form_data:
     paraphraser = get_paraphraser()
 # write the samples
@@ -105,50 +108,90 @@ for d, sf in sample_files:
     sfile_name = sf.replace('ORIG', 'THESIS' if d == Data.THESIS else 'ARXIV' if d == Data.ARXIV else 'WIKIPEDIA')
     sfile = open(os.path.join(path, *['data', 'sample', sampleFolder, sfile_name]), 'w', encoding='utf-8', newline='\n')
 
-    # information in witch order the text occur this sample file
-    sfileInfo = [sfile_name]
+    if return_df:
+        # create DataFrame file
+        dffile_name = 'df_' + sf.replace('ORIG', 'THESIS' if d == Data.THESIS else 'ARXIV' if d == Data.ARXIV else 'WIKIPEDIA')
+        dffile = open(os.path.join(path, *['data', 'sample', sampleFolder, dffile_name]), 'w', encoding='utf-8', newline='\n')
 
-    # shuffle the oder of the percentages
-    shuffle_pc = list(para_pc)
-    random.shuffle(shuffle_pc)
-    for pc in shuffle_pc:
-        sfileInfo.append(pc)# remember the percentage of replaced words
-        if pc == 0:
+    # the order in which the different spun_texts and the original text (index -1) are handeld
+    order = [i for i in range(-1, len(paraphrase_args))]
+
+    if disguise_sample:
+        # information in witch order the text occur this sample file
+        sfileInfo = [sfile_name]
+
+        # shuffle the oder (with -1 for the original text)
+        random.shuffle(order)
+
+    for o in order:
+        if disguise_sample:
+            sfileInfo.append(o)# remember the order
+        else:
+            if o == -1:
+                arg_info_text = "Original Text"
+            else:
+                arg_info_text = ", ".join(['{}={}'.format(arg, paraphrase_args[o][arg]) for arg in paraphrase_args[o]])
+            sfile.write(arg_info_text + ": \n\n")
+
+        if o == -1 or not load_spun_form_data:
             full_path = os.path.join(data_paths[data.index(d)], *['ogUTF-8', sf])
-        elif load_spun_form_data:
-            spun_f = sf.replace('ORIG', 'SPUN')
-            full_path = os.path.join(data_paths[data.index(d)], *['sp({}%)'.format(pc), spun_f])
+        else:
+            spun_f =  sf.replace('ORIG', 'SPUN')
+            # generating the folder name for the spun files
+            spun_dir = "sp("
+            for key in paraphrase_args[o]:
+                if key == 'mask':
+                    spun_dir += "{}%, ".format(int(paraphrase_args[o].get(key) * 100))
+                elif key == 'range_replace':
+                    spun_dir += "{}, ".format(paraphrase_args[o].get(key))
+                elif paraphrase_args[o].get(key) == True:
+                    spun_dir += "{}, ".format(key)
+                else:
+                    spun_dir += "{}: {}, ".format(key, paraphrase_args[o].get(key))
+            spun_dir = spun_dir[:-2] + ")"
 
-            # if the spun text dose not exist then create one
-            if not os.path.isfile(full_path):
-                if paraphraser == None:
-                    paraphraser = get_paraphraser()
-                # spin the original text
-                og_path = os.path.join(data_paths[data.index(d)], *['ogUTF-8', sf])
-                spin_text(paraphraser, og_path, full_path, pc/100)
-        
-        # write into the sample file
-        if load_spun_form_data or pc == 0:
+            full_path = os.path.join(data_paths[data.index(d)], *[spun_dir, spun_f])
+
+        if load_spun_form_data:
             with open(full_path, encoding='utf-8') as file:
                 sfile.write(file.read())
         else:
-            # spin the original text
-            og_path = os.path.join(data_paths[data.index(d)], *['ogUTF-8', sf])
-            sfile.write(spin_text(paraphraser, og_path, None, pc/100))
+            if o == -1:
+                with open(full_path, encoding='utf-8') as file:
+                    sfile.write(file.read())
+            else:
+                spun_text = spin_text(paraphraser, full_path, paraphrase_args[o])
+                if return_df:
+                    spun_text, df = spun_text# split the paraphraser out put in spun_text and DataFrame
+                    
+                    # Write in the DataFrame file
+                    arg_info_text = ", ".join(['{}={}'.format(arg, paraphrase_args[o][arg]) for arg in paraphrase_args[o]])
+                    dffile.write(arg_info_text + ": \n\n")
+                    dffile.write(df.to_string())
+                    dffile.write('\n\n' + 100*'-' + '\n\n')
+
+                sfile.write(spun_text)
+            
         sfile.write('\n\n' + 100*'-' + '\n\n')
 
-    logInfo.append(sfileInfo)
+    if disguise_sample:
+        logInfo.append(sfileInfo)
 
     # close the sample file
     sfile.close()
 
-# create the Info file where the information for the sample is stored
-with open(os.path.join(path, *['data', 'sample', sampleFolder, 'Info.txt']), 'w', encoding='utf-8', newline='\n') as file:
-    if not load_spun_form_data:
-        file.write(', '.join(['{}={}'.format(arg, paraphrase_args[arg]) for arg in paraphrase_args]))
-        file.write('\n\n')
-    for info in logInfo:
-        file.write(info[0] + '\n')
-        for i in range(1, len(info)):
-            file.write("    {}%\n".format(info[i]))
-        file.write('\n')
+    if return_df:
+        # close DataFrame file
+        dffile.close()
+
+if disguise_sample:
+    # create the Info file where the information for the sample is stored
+    with open(os.path.join(path, *['data', 'sample', sampleFolder, 'Info.txt']), 'w', encoding='utf-8', newline='\n') as file:
+        for info in logInfo:
+            file.write(info[0] + '\n')
+            for i in range(1, len(info)):
+                if info[i] == -1:
+                    file.write("    original Text\n")
+                else:
+                    file.write("    " + ", ".join(['{}={}'.format(arg, paraphrase_args[info[i]][arg]) for arg in paraphrase_args[info[i]]]) + "\n")
+            file.write('\n')
